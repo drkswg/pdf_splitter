e80af6e6d2bf7afc1c3418cca1cc089a2051df0c
drkswg <kodian17@gmail.com>
2020-04-22 16:11:21 +0300

    Now with OOP stuff (i guess)

--- pdf_splitter.py
+++ pdf_splitter.py
@@ -8,53 +8,54 @@ from pdfminer.pdfpage import PDFPage
 from PyPDF2 import PdfFileWriter, PdfFileReader
 
 
-def open_file():
-    global file
-    file = fd.askopenfilename()
+class PDFSplitter:
+    def __init__(self):
+        self.open_file()
+        self.get_npers(file)
+        self.split()
 
+    def open_file(self):
+        global file
+        file = fd.askopenfilename()
 
-def get_pages(pdf_path):
-    with open(pdf_path, 'rb') as fh:
-        for page in PDFPage.get_pages(fh,
-                                      caching=True,
-                                      check_extractable=True):
-            resource_manager = PDFResourceManager()
-            fake_file_handle = io.StringIO()
-            converter = TextConverter(resource_manager, fake_file_handle)
-            page_interpreter = PDFPageInterpreter(resource_manager, converter)
-            page_interpreter.process_page(page)
+    def get_pages(self, pdf_path):
+        with open(pdf_path, 'rb') as fh:
+            for page in PDFPage.get_pages(fh,
+                                          caching=True,
+                                          check_extractable=True):
+                resource_manager = PDFResourceManager()
+                fake_file_handle = io.StringIO()
+                converter = TextConverter(resource_manager, fake_file_handle)
+                page_interpreter = PDFPageInterpreter(resource_manager, converter)
+                page_interpreter.process_page(page)
 
-            text = fake_file_handle.getvalue()
-            yield text
+                text = fake_file_handle.getvalue()
+                yield text
 
-            converter.close()
-            fake_file_handle.close()
+                converter.close()
+                fake_file_handle.close()
 
+    def get_npers(self, pdf_path):
+        global s
+        s = []
 
-def get_npers(pdf_path):
-    global s
-    s = []
+        for page in self.get_pages(pdf_path):
+            npers = re.search('\d\d\d-\d\d\d-\d\d\d\s\d\d', page)
+            try:
+                s.append(npers.group(0))
+            except:
+                s.append('?')
 
-    for page in get_pages(pdf_path):
-        npers = re.search('\d\d\d-\d\d\d-\d\d\d\s\d\d', page)
-        try:
-            s.append(npers.group(0))
-        except:
-            s.append('?')
+    def split(self):
+        f = PdfFileReader(open(file, 'rb'))
 
+        for i in range(f.numPages):
+            output = PdfFileWriter()
+            output.addPage(f.getPage(i))
 
-def split():
-    f = PdfFileReader(open(file, 'rb'))
-
-    for i in range(f.numPages):
-        output = PdfFileWriter()
-        output.addPage(f.getPage(i))
-
-        with open("pages/{0}) {1}.pdf".format(i, s[i]), "wb") as outputStream:
-            output.write(outputStream)
+            with open(f"pages/{i}) {s[i]}.pdf", "wb") as outputStream:
+                output.write(outputStream)
 
 
 if __name__ == '__main__':
-    open_file()
-    get_npers(file)
-    split()
+    PDFSplitter()
